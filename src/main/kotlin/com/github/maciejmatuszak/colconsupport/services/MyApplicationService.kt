1702c36ec18e74a524acd4699ce9adfa1d0c10a8
GitHub Action <action@github.com>
2021-10-27 09:17:52 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/maciejmatuszak/colconsupport/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.maciejmatuszak.colconsupport.services
+
+import com.github.maciejmatuszak.colconsupport.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
